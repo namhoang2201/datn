d0ed985b35f2f53450ca0d05e6d13a4f5cff8da1
namhoang2201 <hoangnamtb95@gmail.com>
2020-05-10 12:00:19 +0700

    create new module: Nam_RewardPoint and setup database

---
+++ extensions/Nam/RewardPoint/Setup/InstallSchema.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Nam\Rewardpoint\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\DB\Ddl\Table;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    /**
+     * Installs DB schema for a module
+     *
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+        $installer->startSetup();
+
+        /**
+         * Drop tables if exists
+         */
+        $installer->getConnection()->dropTable($installer->getTable('namrewardpoint_transaction'));
+
+        /**
+         * Create table 'namrewardpoint_transaction'
+         */
+        $table = $installer->getConnection()
+            ->newTable($installer->getTable('namrewardpoint_transaction'))
+            ->addColumn(
+                'transaction_id',
+                Table::TYPE_INTEGER,
+                null,
+                ['identity' => true, 'nullable' => false, 'primary' => true, 'auto_increment' => true],
+                'Transaction Id'
+            )->addColumn(
+                'np_order_id',
+                Table::TYPE_INTEGER,
+                null,
+                ['unsigned'=>true, 'nullable'=>false, 'default' => '0'],
+                'nam_rewardpoint_order_id reference to sale_order.order_id'
+            )
+            ->addColumn(
+                'point_before_transaction',
+                Table::TYPE_INTEGER,
+                null,
+                ['nullable' => true, 'default' => 0],
+                'Points of customer before transaction'
+            )->addColumn(
+                'point_earn',
+                Table::TYPE_INTEGER,
+                null,
+                ['nullable' => false, 'default' => 0],
+                'Point amount that customer earned when create current transaction'
+            )->addColumn(
+                'point_spend',
+                Table::TYPE_INTEGER,
+                null,
+                ['nullable' => true],
+                'Point amount that customer spent when create current transaction'
+            )->addColumn(
+                'total_before',
+                Table::TYPE_DECIMAL,
+                null,
+                ['nullable' => false, 'default' => 0.00],
+                'Grand total before discount'
+            )->addColumn(
+                'discount_amount',
+                Table::TYPE_DECIMAL,
+                null,
+                ['nullable' => true, 'default' => 0.00],
+                'Discount amount when spent point'
+            )->addColumn(
+                'total_after_discount',
+                Table::TYPE_DECIMAL,
+                null,
+                ['nullable' => false, 'default' => 0.00],
+                'Final Grand Total'
+            )->addColumn(
+                'created_time',
+                Table::TYPE_DATETIME,
+                null,
+                [],
+                'Time create transaction'
+            )->addForeignKey(
+                $installer->getFkName(
+                    'namrewardpoint_transaction',
+                    'np_order_id',
+                    'sales_order',
+                    'entity_id'
+                ),
+                'np_order_id',
+                $installer->getTable('sales_order'),
+                'entity_id',
+                Table::ACTION_SET_DEFAULT
+            );
+        $installer->getConnection()->createTable($table);
+
+        /**
+         * Add 1 column 'reward_point' to table  'customer_entity'
+         */
+        $installer->getConnection()->addColumn(
+            $installer->getTable('customer_entity'),
+            'reward_point',
+            [
+                'type' => Table::TYPE_INTEGER,
+                'nullable' => true,
+                'default' => 0,
+                'comment' => 'Current balance point of customer'
+            ]
+        );
+
+        /**
+         * Add 2 column 'earn_point', 'point_spend' to table  'sales_order'
+         */
+        $installer->getConnection()->addColumn(
+            $installer->getTable('sales_order'),
+            'point_earn',
+            [
+                'type' => Table::TYPE_INTEGER,
+                'nullable' => true,
+                'default' => 0,
+                'comment' => 'Point amount that customer earned from current order'
+            ]
+        );
+        $installer->getConnection()->addColumn(
+            $installer->getTable('sales_order'),
+            'point_spend',
+            [
+                'type' => Table::TYPE_INTEGER,
+                'nullable' => true,
+                'default' => 0,
+                'comment' => 'Point amount that customer spent at current order'
+            ]
+        );
+
+        /**
+         * Add 1 column 'namrewardpoint_discount_amount' to table 'quote'
+         */
+        $installer->getConnection()->addColumn(
+            $installer->getTable('quote'),
+            'namrewardpoint_discount_amount',
+            [
+                'type' => Table::TYPE_DECIMAL,
+                'length' => '12,4',
+                'nullable' => true,
+                'default' => 0.00,
+                'comment' => 'Save discount amount from calculating point customer use from pwa studio then save to quote table'
+            ]
+        );
+
+        $installer->endSetup();
+    }
+}
