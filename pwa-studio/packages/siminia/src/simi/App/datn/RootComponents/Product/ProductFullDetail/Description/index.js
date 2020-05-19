b1768a3eb35da9e7654ab63ec4ba0e1398149a2f
namhoang2201 <hoangnamtb95@gmail.com>
2020-05-19 14:35:35 +0700

    complete generate QrCode for each product

--- pwa-studio/packages/siminia/src/simi/App/datn/RootComponents/Product/ProductFullDetail/Description/index.js
+++ pwa-studio/packages/siminia/src/simi/App/datn/RootComponents/Product/ProductFullDetail/Description/index.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import Identify from 'src/simi/Helper/Identify';
 import RichText from 'src/simi/BaseComponents/RichText';
+import QRCode from "qrcode.react";
 
 require('./description.scss')
 
 const Description = props => {
-    const {product} = props
+    const { product } = props
+    console.log(product)
     return (
         <React.Fragment>
             <h2 className="description-title">
                 <span>{Identify.__('Description')}</span>
             </h2>
             <RichText className="description-content" content={product.description.html} />
+            <div className="qr">
+                <QRCode
+                    className="qrcode-product"
+                    id='qrcode'
+                    value={product.id}
+                    size={290}
+                    level={'H'}
+                    includeMargin={true}
+                />
+            </div>
         </React.Fragment>
     )
 }
